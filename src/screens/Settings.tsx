fc7becc049c819e0047883b91718708199424203
Khairunnisa <adityasubono@gmail.com>
2024-03-25 04:12:41 +0700

    first commit

---
+++ src/screens/Settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+function Setting() {
+  return (
+    <View>
+      <Text>Setting Screen</Text>
+    </View>
+  );
+}
+
+export default Setting;
